194031c1172eb6039d4709f85b024d3a71063f01
Peter Holloway <holloway.p.r@gmail.com>
2024-12-09 19:52:24 +0000

    Replace BTreeMaps and BTreeSets with Vec(Deques)
    Where possible. Reduces runtime from ~15ms to ~3ms.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{BTreeSet, VecDeque};
 
 use aoc24::input;
 
@@ -11,18 +11,22 @@ pub fn main() {
     println!("Part 2: {checksum}");
 }
 
-fn shuffle_blocks(disk: &str) -> u64 {
-    let mut disk_map = disk.trim().chars().map(|c| c.to_digit(10).unwrap() as u64);
-    let mut mem = BTreeMap::<u64, u64>::new();
-    let mut free = BTreeSet::new();
+fn shuffle_blocks(disk: &str) -> usize {
+    let mut disk_map = disk
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize);
+    let mut mem = Vec::new();
+    let mut free = VecDeque::new();
     let mut idx = 0;
     let mut id = 0;
+    let mut checksum = 0;
     loop {
         let Some(len) = disk_map.next() else {
             break;
         };
         for i in idx..idx + len {
-            mem.insert(i, id);
+            mem.push((i, id));
         }
         idx += len;
         id += 1;
@@ -30,81 +34,82 @@ fn shuffle_blocks(disk: &str) -> u64 {
             break;
         };
         for i in idx..idx + len {
-            free.insert(i);
+            free.push_back(i);
         }
         idx += len;
     }
 
-    while free
-        .first()
-        .is_some_and(|f| f < mem.last_key_value().unwrap().0)
-    {
-        let tgt = free.pop_first().unwrap();
-        let blk = mem.pop_last().unwrap();
-        mem.insert(tgt, blk.1);
+    while free.front().is_some_and(|f| f < &mem.last().unwrap().0) {
+        let tgt = free.pop_front().unwrap();
+        let blk = mem.pop().unwrap();
+        checksum += tgt * blk.1;
     }
 
-    let checksum = mem.iter().map(|(k, v)| k * v).sum::<u64>();
+    checksum += mem.iter().map(|(k, v)| k * v).sum::<usize>();
     checksum
 }
 
-fn shuffle_ids(disk: &str) -> u64 {
-    let mut mem = BTreeMap::<u32, Block>::new();
-    let mut free = BTreeMap::<u32, BTreeSet<u32>>::new();
+fn shuffle_ids(disk: &str) -> usize {
+    let mut mem = Vec::new();
+    let mut free = vec![BTreeSet::new(); 10];
     let mut idx = 0;
-    let mut id = 0;
-    let mut disk_map = disk.trim().chars().map(|c| c.to_digit(10).unwrap());
+    let mut disk_map = disk
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize);
     loop {
         let Some(len) = disk_map.next() else {
             break;
         };
-        mem.insert(id, Block { len, idx });
+        mem.push(Block { len, idx });
         idx += len;
-        id += 1;
         let Some(len) = disk_map.next() else {
             break;
         };
-        free.entry(len).or_default().insert(idx);
+        free[len].insert(idx);
         idx += len;
     }
-    for i in (0..id).rev() {
-        let blk = mem.get_mut(&i).unwrap();
+    let mut checksum = 0;
+    for (id, mut blk) in mem.into_iter().enumerate().rev() {
         // For every memory span long enough for the block, find the earliest index
         // Of these, find the first, and check it is earlier than the block's current position
         let Some((len, idx)) = free
-            .range(blk.len..)
-            .flat_map(|(len, opts)| opts.first().map(|idx| (*len, *idx)))
+            .iter()
+            .enumerate()
+            .skip(blk.len as usize)
+            .flat_map(|(len, opts)| opts.first().map(|idx| (len, *idx)))
             .min_by_key(|(_, idx)| *idx)
             .filter(|(_, idx)| idx < &blk.idx)
         else {
             // There is no suitable free space
+            checksum += blk.checksum(id);
             continue;
         };
         // Remove the space from the free map
-        free.get_mut(&len).unwrap().remove(&idx);
+        free[len].remove(&idx);
 
         // Move the block
         blk.idx = idx;
+        checksum += blk.checksum(id);
 
         // Add the remaining free space back to map
         if len > blk.len {
-            free.entry(len - blk.len).or_default().insert(idx + blk.len);
+            free[len - blk.len].insert(idx + blk.len);
         }
     }
-    let checksum = mem.iter().map(|(id, blk)| blk.checksum(*id)).sum::<u64>();
     checksum
 }
 
 #[derive(Debug)]
 struct Block {
-    idx: u32,
-    len: u32,
+    idx: usize,
+    len: usize,
 }
 
 impl Block {
-    fn checksum(&self, id: u32) -> u64 {
+    fn checksum(&self, id: usize) -> usize {
         (self.idx..self.idx + self.len)
-            .map(|i| i as u64 * id as u64)
-            .sum::<u64>()
+            .map(|i| (i * id))
+            .sum::<usize>()
     }
 }
