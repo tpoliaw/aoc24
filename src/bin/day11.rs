0d55d96bc16cd757127dbc563605436f6722b1a0
Peter Holloway <holloway.p.r@gmail.com>
2024-12-11 07:52:14 +0000

    Day 11 - Part 2 the less naive way
    The naive approach got to about 60 steps before running out of memory

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,38 +1,62 @@
+use std::{collections::HashMap, mem};
+
 use aoc24::input;
 
 pub fn main() {
-    let mut stones = input(11)
+    let stones = input(11)
         .as_value::<String>()
         .trim()
         .split_whitespace()
         .map(|n| n.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
 
-    blink(25, &mut stones);
-    println!("Part 1: {}", stones.len());
+    let count = blink(25, &stones);
+    println!("Part 1: {}", count);
+    let count = blink(75, &stones);
+    println!("Part 2: {}", count);
 }
 
-fn blink(count: u64, stones: &mut Vec<u64>) {
-    let mut next = Vec::new();
+enum Step {
+    One(u64),
+    Two(u64, u64),
+}
+
+fn blink(count: u32, stones: &[u64]) -> u64 {
+    let mut counts: HashMap<u64, u64> = HashMap::new();
+    for s in stones {
+        *counts.entry(*s).or_default() += 1;
+    }
+
+    let mut next = HashMap::new();
     for _ in 0..count {
-        for s in stones.iter() {
-            if *s == 0 {
-                next.push(1);
-            } else {
-                let d = digits(*s);
-                if d % 2 == 0 {
-                    next.push(s.div_euclid(10u64.pow(d / 2)));
-                    next.push(s.rem_euclid(10u64.pow(d / 2)));
-                } else {
-                    next.push(*s * 2024);
+        for (s, cnt) in counts.iter() {
+            match step(*s) {
+                Step::One(n) => *next.entry(n).or_default() += cnt,
+                Step::Two(n, m) => {
+                    *next.entry(n).or_default() += cnt;
+                    *next.entry(m).or_default() += cnt;
                 }
             }
         }
-        stones.clear();
-        std::mem::swap(stones, &mut next);
+        mem::swap(&mut counts, &mut next);
+        next.clear();
     }
+
+    counts.values().sum::<u64>()
 }
 
-fn digits(n: u64) -> u32 {
-    n.ilog10() + 1
+fn step(stone: u64) -> Step {
+    if stone == 0 {
+        Step::One(1)
+    } else {
+        let d = stone.ilog10() + 1;
+        if d % 2 == 0 {
+            Step::Two(
+                stone.div_euclid(10u64.pow(d / 2)),
+                stone.rem_euclid(10u64.pow(d / 2)),
+            )
+        } else {
+            Step::One(stone * 2024)
+        }
+    }
 }
