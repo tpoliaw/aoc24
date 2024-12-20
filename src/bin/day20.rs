9a8a035fdf6a457fe65b1bf0c168bc1c4be3786b
Peter Holloway <holloway.p.r@gmail.com>
2024-12-20 14:53:27 +0000

    Day 20 - Reimplemented the quick way
    Including optimisation approach from reddit but written from scratch
    after realisation that the track is a single path from start to finish
    and there are no forks.
    
    General idea:
    * Find list of cells making up track
    * For each cell on track, start 100 (minimum cheat threshold) ahead and
      check each cell in turn for whether the direct distance is more than
      100 less than the track distance.
    * If the checked cell is not within the cheat distance, jump forward the
      difference between its distance and the allowed distance as none of
      the intermediate cells can be within the distance either.
    
    Runtime down to ~20ms for both parts.

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{BTreeMap, BTreeSet, HashMap, HashSet},
-    ops::Add,
-};
+use std::{collections::HashSet, ops::Add};
 
 use aoc24::input;
 
@@ -22,79 +19,56 @@ pub fn main() {
         .collect::<Vec<_>>();
 
     let walls = all.iter().cloned().collect::<HashSet<_>>();
-    println!("Walls: {}", walls.len());
 
     let width = src.find('\n').unwrap();
     let start = src.find('S').unwrap();
-    let end = src.find('E').unwrap();
     let start = Pos {
         row: (start / (width + 1)) as i32,
         col: (start % (width + 1)) as i32,
     };
-    let end = Pos {
-        row: (end / (width + 1)) as i32,
-        col: (end % (width + 1)) as i32,
-    };
 
-    let from_start = min_dists(start, end, &walls);
-    let from_end = min_dists(end, start, &walls);
-    assert!(from_end[&start] == from_start[&end]);
+    let track = plot_track(start, &walls);
 
-    let baseline = from_end[&start];
-    println!("Baseline: {baseline}");
-
-    let p1 = count_cheats(&from_start, &from_end, baseline, 2);
+    let p1 = count_cheats(&track, 2);
     println!("Part 1: {p1}");
-    let p2 = count_cheats(&from_start, &from_end, baseline, 20);
+
+    let p2 = count_cheats(&track, 20);
     println!("Part 2: {p2}");
 }
 
-fn count_cheats(
-    from_start: &BTreeMap<Pos, usize>,
-    from_end: &BTreeMap<Pos, usize>,
-    baseline: usize,
-    skips: u32,
-) -> usize {
-    let mut cheats: HashMap<usize, usize> = HashMap::new();
-    for (cell, sdist) in from_start.iter() {
-        for (end, edist) in from_end
-            .iter()
-            .filter(|(_, v)| **v < baseline - sdist - skips as usize)
-        {
-            let offset = cell.row.abs_diff(end.row) + cell.col.abs_diff(end.col);
-            if offset <= skips {
-                let dist = sdist + edist + offset as usize;
-                if dist < baseline {
-                    *cheats.entry(baseline - dist).or_default() += 1;
-                }
+fn count_cheats(track: &[Pos], skips: usize) -> usize {
+    let mut cheats = 0;
+
+    for (dist, cell) in track.iter().enumerate() {
+        let mut check = dist + 100;
+        while check < track.len() {
+            let end = track[check];
+            let d = cell.dist(&end);
+            if d <= skips && dist + d + 100 <= check {
+                cheats += 1;
             }
+            check += d.saturating_sub(skips).max(1);
         }
     }
     cheats
-        .iter()
-        .filter(|(k, _)| **k >= 100)
-        .map(|(_, v)| v)
-        .sum::<usize>()
 }
 
-fn min_dists(start: Pos, end: Pos, walls: &HashSet<Pos>) -> BTreeMap<Pos, usize> {
-    let mut options: BTreeSet<(usize, Pos)> = [(0, start)].into();
-    let mut visited = BTreeMap::new();
-    while let Some((d, pos)) = options.pop_first() {
-        if visited.get(&pos).is_some_and(|v| *v < d) {
-            continue;
-        }
+fn plot_track(start: Pos, walls: &HashSet<Pos>) -> Vec<Pos> {
+    let mut prev = start;
+    let mut next = Some(start);
+    let mut track = vec![];
+    while let Some(pos) = next {
         if walls.contains(&pos) {
             continue;
         }
-        visited.insert(pos, d);
-        if end == pos {
-            break;
-        }
-        options.extend(Dir::each().map(|dr| (d + 1, pos + dr)));
+        track.push(pos);
+        next = Dir::each()
+            .map(|dr| pos + dr)
+            .filter(|c| c != &prev && !walls.contains(c))
+            .next();
+        prev = pos;
     }
-
-    visited
+    track
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
@@ -115,21 +89,8 @@ impl Dir {
     }
 }
 impl Pos {
-    fn from_line(ln: String) -> Self {
-        let (r, c) = ln.split_once(',').unwrap();
-        Self {
-            row: r.parse().unwrap(),
-            col: c.parse().unwrap(),
-        }
-    }
-    fn in_limits(&self, limit: Pos) -> bool {
-        0 <= self.row && self.row <= limit.row && 0 <= self.col && self.col <= limit.col
-    }
-    fn neighbours(&self, walls: &HashSet<Pos>) -> usize {
-        Dir::each()
-            .map(|d| *self + d)
-            .filter(|p| walls.contains(p))
-            .count()
+    fn dist(&self, other: &Self) -> usize {
+        (self.row.abs_diff(other.row) + self.col.abs_diff(other.col)) as usize
     }
 }
 
