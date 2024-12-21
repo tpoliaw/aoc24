fdb37324c0f9b9bfdb25c8011d8eec14e7edfa23
Peter Holloway <holloway.p.r@gmail.com>
2024-12-21 21:36:55 +0000

    Day 21 - Part 2, cache *everything*
    Part 1 also re-implemented to use the same approach.

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 use aoc24::input;
@@ -21,14 +19,17 @@ use aoc24::input;
 // +---+---+---+
 
 pub fn main() {
-    let codes = input(21)
-        .map_by_line(|ln| {
+    let src = input(21).string();
+    let codes = src
+        .lines()
+        .map(|ln| {
             (
                 ln[..ln.len() - 1].parse::<usize>().unwrap(),
                 ln.chars().collect::<Vec<_>>(),
             )
         })
         .collect::<Vec<_>>();
+
     let digits = [
         ('7', (0, 0)),
         ('8', (0, 1)),
@@ -45,18 +46,7 @@ pub fn main() {
     .into_iter()
     .collect::<HashMap<_, _>>();
 
-    let arrows = [
-        ('^', (0, 1)),
-        ('A', (0, 2)),
-        ('<', (1, 0)),
-        ('v', (1, 1)),
-        ('>', (1, 2)),
-    ]
-    .into_iter()
-    .collect::<HashMap<_, _>>();
-
     let mut digit_map = map_pairs(&digits);
-    let mut arrow_map = map_pairs(&arrows);
 
     digit_map
         .iter_mut()
@@ -95,66 +85,125 @@ pub fn main() {
                     || p.get(2).is_none_or(|m| *m != Move::Down)
             })
         });
+    let digit_map = digit_map
+        .into_iter()
+        .map(|(k, mut v)| {
+            (k, {
+                v.sort_unstable();
+                v.remove(0)
+            })
+        })
+        .collect::<HashMap<(char, char), Vec<Move>>>();
 
-    arrow_map // ^ -> <
-        .iter_mut()
-        .filter(|((b1, _), _)| *b1 == '^')
-        .for_each(|(_, v)| v.retain(|p| p.first().is_none_or(|m| *m != Move::Left)));
-    arrow_map // A -> <
-        .get_mut(&('A', '<'))
-        .unwrap()
-        .retain(|p| *p.first().unwrap() == Move::Left);
-    arrow_map // < -> ^/A
-        .iter_mut()
-        .filter(|((b1, _), _)| *b1 == '<')
-        .for_each(|(_, v)| v.retain(|p| p.first().is_none_or(|m| *m != Move::Up)));
+    // moves should be grouped and sorted left to right where possible, the blank space means this
+    // isn't always possible
+    let arrow_map: HashMap<(char, char), Vec<Move>> = [
+        // A -> ?
+        (('A', 'A'), ""),
+        (('A', '<'), "v<<"), // blank space
+        (('A', 'v'), "<v"),
+        (('A', '>'), "v"),
+        (('A', '^'), "<"),
+        // < -> ?
+        (('<', 'A'), ">>^"), // blank space
+        (('<', '<'), ""),
+        (('<', 'v'), ">"),
+        (('<', '^'), ">^"), // blank space
+        // v -> ?
+        (('v', 'A'), "^>"),
+        (('v', '<'), "<"),
+        (('v', 'v'), ""),
+        (('v', '>'), ">"),
+        // > -> ?
+        (('>', 'A'), "^"),
+        (('>', 'v'), "<"),
+        (('>', '>'), ""),
+        (('>', '^'), "<^"),
+        // ^ -> ?
+        (('^', 'A'), ">"),
+        (('^', '<'), "v<"),
+        (('^', '>'), "v>"), // blank space
+        (('^', '^'), ""),
+    ]
+    .into_iter()
+    .map(|(p, v)| (p, v.chars().map(Move::from_char).collect::<Vec<_>>()))
+    .collect();
+
+    let codes = codes
+        .into_iter()
+        .map(|(k, v)| (k, paths(&v, &digit_map, &mut HashMap::new())))
+        .collect::<Vec<_>>();
+
+    let mut pair_cache: HashMap<Vec<char>, Vec<Vec<char>>> = Default::default();
+    let mut len_cache = HashMap::new();
 
     let mut complex = 0;
-
-    for (num, code) in codes {
-        let mut all = vec![];
-        let codes = paths(&code, &digit_map);
-        for code in codes {
-            let codes = paths(&code, &arrow_map);
-            for code in codes {
-                let codes = paths(&code, &arrow_map);
-                all.extend(codes);
-            }
+    for (num, code) in &codes {
+        let mut count = 0;
+        for segment in code {
+            let len = chained(segment, &arrow_map, &mut pair_cache, &mut len_cache, 2);
+            count += len;
         }
-        let min = all.iter().min_by_key(|c| c.len()).unwrap();
-        complex += min.len() * num;
+        complex += count * num;
     }
     println!("Part 1: {complex}");
+
+    let mut complex = 0;
+    for (num, code) in &codes {
+        let mut count = 0;
+        for segment in code {
+            let len = chained(segment, &arrow_map, &mut pair_cache, &mut len_cache, 25);
+            count += len;
+        }
+        complex += count * num;
+    }
+    println!("Part 2: {complex}");
 }
 
-fn paths(code: &[char], pairs: &HashMap<(char, char), Vec<Vec<Move>>>) -> Vec<Vec<char>> {
+fn chained(
+    code: &[char],
+    pairs: &HashMap<(char, char), Vec<Move>>,
+    pair_cache: &mut HashMap<Vec<char>, Vec<Vec<char>>>,
+    len_cache: &mut HashMap<(Vec<char>, usize), usize>,
+    steps: usize,
+) -> usize {
+    if steps == 0 {
+        return code.len();
+    }
+    if let Some(l) = len_cache.get(&(code.to_vec(), steps)) {
+        return *l;
+    }
+    let segments = paths(code, pairs, pair_cache);
+    let len = segments
+        .iter()
+        .map(|s| chained(&s, pairs, pair_cache, len_cache, steps - 1))
+        .sum();
+    len_cache.insert((code.to_vec(), steps), len);
+    len
+}
+
+fn paths(
+    code: &[char],
+    pairs: &HashMap<(char, char), Vec<Move>>,
+    cache: &mut HashMap<Vec<char>, Vec<Vec<char>>>,
+) -> Vec<Vec<char>> {
+    if let Some(p) = cache.get(code) {
+        return p.clone();
+    }
     let mut prev = 'A';
-    let mut paths: Vec<Vec<Move>> = vec![vec![]];
+    let mut paths: Vec<Vec<Move>> = vec![];
     for b in code {
-        let seqs = &pairs[&(prev, *b)];
-        match &seqs[..] {
-            [] => {}
-            [one] => paths.iter_mut().for_each(|p| p.extend(one)),
-            [one, two] => {
-                paths = paths
-                    .into_iter()
-                    .flat_map(|mut p| {
-                        let mut left = p.clone();
-                        left.extend(one);
-                        p.extend(two);
-                        [p, left]
-                    })
-                    .collect()
-            }
-            _ => panic!(),
-        }
-        paths.iter_mut().for_each(|p| p.push(Move::Press));
+        let seq = &pairs[&(prev, *b)];
+        paths.push(seq.clone());
         prev = *b;
     }
-    paths
+    paths.iter_mut().for_each(|p| p.push(Move::Press));
+    let paths: Vec<Vec<char>> = paths
         .into_iter()
         .map(|p| p.into_iter().map(|m| m.to_char()).collect())
-        .collect()
+        .collect();
+    cache.insert(code.into(), paths.clone());
+    paths
 }
 
 fn map_pairs(buttons: &HashMap<char, (u8, u8)>) -> HashMap<(char, char), Vec<Vec<Move>>> {
@@ -162,7 +211,7 @@ fn map_pairs(buttons: &HashMap<char, (u8, u8)>) -> HashMap<(char, char), Vec<Vec
     for (b1, (r1, c1)) in buttons {
         for (b2, (r2, c2)) in buttons {
             if b1 == b2 {
-                pairs.insert((*b1, *b2), vec![]);
+                pairs.insert((*b1, *b2), vec![vec![]]);
                 continue;
             }
 
@@ -193,13 +242,13 @@ fn map_pairs(buttons: &HashMap<char, (u8, u8)>) -> HashMap<(char, char), Vec<Vec
     pairs
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 enum Move {
-    Right,
-    Up,
-    Down,
-    Left,
-    Press,
+    Press = 0,
+    Left = 1,
+    Down = 2,
+    Up = 3,
+    Right = 4,
 }
 
 impl Move {
@@ -210,6 +259,16 @@ impl Move {
             Move::Down => 'v',
             Move::Left => '<',
             Move::Press => 'A',
+        }
+    }
+    fn from_char(c: char) -> Self {
+        match c {
+            '>' => Self::Right,
+            '<' => Self::Left,
+            'v' => Self::Down,
+            '^' => Self::Up,
+            'A' => Self::Press,
+            _ => panic!(),
         }
     }
 }
